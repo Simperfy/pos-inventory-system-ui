dba85c966169e06d585829ba64b1cab7c475353e
Simperfy <28738855+Simperfy@users.noreply.github.com>
2020-12-29 20:48:37 +0800

    UPDATED: Search bar

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -5,32 +5,50 @@ import { ReactComponent as Magnify } from '../../assets/icons/magnify.svg';
 
 import './style.css';
 
-function SearchBar() {
-  const results = [
-    { name: 'item 1', barcode: '5fe2ff51ab328745dc231241' },
-    { name: 'item 2', barcode: '5fe2ff51ab328745dc231242' },
-    { name: 'item 3', barcode: '5fe2ff51ab328745dc231243' },
-  ];
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div className="search-group">
-      <div className="search-bar">
-        <input
-          className="search-bar-input"
-          type="text"
-          placeholder="Item / 5fe2ff51ab328745dc231241"
-        />
-        {/* <img src={Magnify} alt="search icon"/> */}
-        <button type="button" className="search-btn align-text-bottom">
-          <Magnify className="search-icon" />
-        </button>
+    this.state = {
+      results: [
+        { name: 'item 1', barcode: '5fe2ff51ab328745dc231241' },
+        { name: 'item 2', barcode: '5fe2ff51ab328745dc231242' },
+        { name: 'item 3', barcode: '5fe2ff51ab328745dc231243' },
+      ],
+      showResults: false,
+    };
+  }
+
+  handleChange = (e) => {
+    if (e.target.value) this.setState({ showResults: true });
+    else this.setState({ showResults: false });
+  };
+
+  render() {
+    return (
+      <div className="search-group">
+        <div className="search-bar">
+          <input
+            className="search-bar-input"
+            type="text"
+            placeholder="Item / 5fe2ff51ab328745dc231241"
+            onChange={this.handleChange}
+          />
+          <button type="button" className="search-btn align-text-bottom">
+            <Magnify className="search-icon" />
+          </button>
+        </div>
+
+        {this.state.showResults && (
+          <div style={{ position: 'fixed', width: '65%' }}>
+            {this.state.results.map((res) => (
+              <SearchBarItem name={res.name} barcode={res.barcode} />
+            ))}
+          </div>
+        )}
       </div>
-
-      {results.map((res) => (
-        <SearchBarItem name={res.name} barcode={res.barcode} />
-      ))}
-    </div>
-  );
+    );
+  }
 }
 
 export default SearchBar;
